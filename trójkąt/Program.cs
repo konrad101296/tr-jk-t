c682daa7f9fe29c3d60f6f05dc05b12fba798e8d
konrad101296 <konradslimak@o2.pl>
2024-02-23 21:18:32 +0100

    Add project files.

---
+++ trójkąt/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("podaj wysokość:");
+int wysokosc = int.Parse(Console.ReadLine());
+int szerokosc = 0;
+for (int i = 0; i < wysokosc; i++)
+{
+    szerokosc++;
+    Console.WriteLine();
+    for (int j = 0; j < szerokosc; j++)
+    {
+        Console.Write(" *");
+    }
+
+}
